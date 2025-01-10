a3758134813500652cb7933601d2b101b0517775
Daniel <daniel.mg1705@gmail.com>
2025-01-10 09:38:21 +0100

    9:38

--- api/api.php
+++ api/api.php
@@ -1,11 +1,30 @@
 <?php
-$url = " https://v2.jokeapi.dev/";
+$url = "https://www.swapi.tech/api/people/1";
 $response = file_get_contents($url);
 
-if ($response !== false){
+if ($response !== false) {
     $data = json_decode($response, true);
-    print_r($data);
+    $person = $data['result']['properties'];
+
+    echo "<table border='1' style='width: 70%; margin: 20px auto; text-align: left;'>";
+    echo "<thead>";
+    echo "<tr style='background-color: #f2f2f2;'>";
+    echo "<th style='padding: 10px;'>Clave</th>";
+    echo "<th style='padding: 10px;'>Valor</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    foreach ($person as $key => $value) {
+        echo "<tr>";
+        echo "<td style='padding: 10px;'>$key</td>";
+        echo "<td style='padding: 10px;'>$value</td>";
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
 } else {
-    echo"No se pudo obtener informacion";
+    echo "<p>No se pudo obtener información</p>";
 }
 ?>
